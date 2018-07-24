611d38739d2a493fb4581484add56ef31152b8c2
Brad McCollum <btmcco8@gmail.com>
2018-07-24 20:37:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
     "FizzBuzz"
-    elsif int % 3 != 0 && int % 5 != 0
-    "nil"
     elsif int % 3 == 0 
-    "Fizz"
-  else int % 5 == 0 
-  "Buzz"
-end
+      "Fizz"
+    elsif int % 5 == 0 
+      "Buzz"
+    else int % 3 != 0 || int % 5 != 0
+      "nil"
+  end
 end
